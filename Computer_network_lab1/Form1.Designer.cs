5aaa8d0198bc1e3381f79a2b2a9b1cb06855a718
Wurdy <timur.berdieff@yandex.ru>
2021-09-17 22:15:17 +0400

    Добавьте файлы проекта.

---
+++ Computer_network_lab1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Computer_network_lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.TrackBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(237, 618);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(185, 36);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // TrackBar
+            // 
+            this.TrackBar.Location = new System.Drawing.Point(629, 12);
+            this.TrackBar.Maximum = 1000000;
+            this.TrackBar.Minimum = 1000;
+            this.TrackBar.Name = "TrackBar";
+            this.TrackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.TrackBar.Size = new System.Drawing.Size(45, 600);
+            this.TrackBar.TabIndex = 2;
+            this.TrackBar.Value = 1000;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(552, 630);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 666);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TrackBar);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.TrackBar TrackBar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Computer_network_lab1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Computer_network_lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double a = 1;
+        double b = 1;
+
+        int h = 0;
+        private void CalculateAreaUnderCurve()
+        {
+
+        }
+
+        // y = f(x) кривая
+
+        //Сгенерировать случайную точку на промежутке от 0 до 1
+
+        //Проверить попала ли точка в площадь фигуры y<=f(x), если да то h+1
+
+        //Пройдя N итераций вычисляем h/N это и есть интеграл
+        // Use Monte Carlo simulation to estimate pi.
+        private void MonteCarloPi()
+        {
+            long N = Convert.ToInt64(TrackBar.Value);
+            Random rand = new Random();
+
+            // Make a bitmap to show points.
+            int wid = pictureBox1.ClientSize.Width;
+            int hgt = pictureBox1.ClientSize.Height;
+            Bitmap bm = new Bitmap(wid, hgt);
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                gr.Clear(Color.White);
+                gr.DrawEllipse(Pens.Black, 0, 0, wid - 1, hgt - 1);
+            }
+
+            // Make the random points.
+            int numHits = 0;
+            for (int i = 0; i < N; i++)
+            {
+                // Make a random point 0 <= x < 1.
+                double x = rand.NextDouble();
+                double y = rand.NextDouble();
+
+                // See how far the point is from (0.5, 0.5).
+                double dx = x - 0.5;
+                double dy = y - 0.5;
+                if (dx * dx + dy * dy < 0.25) numHits++;
+
+                // Plots up to 10,000 points.
+                if (i < 10000)
+                {
+                    int ix = (int)(wid * x);
+                    int iy = (int)(hgt * y);
+                    if (dx * dx + dy * dy < 0.25)
+                        bm.SetPixel(ix, iy, Color.Gray);
+                    else
+                        bm.SetPixel(ix, iy, Color.Black);
+                }
+            }
+
+            // Display the plotted points.
+            pictureBox1.Image = bm;
+
+
+            // Get the hit fraction.
+            double fraction = numHits / (double)N;
+
+            // Estimate pi.
+            double output = 4.0 * fraction;
+            label1.Text = output.ToString();
+
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            Thread ChecksumsThread = new Thread(new ThreadStart(MonteCarloPi));
+            ChecksumsThread.Priority = ThreadPriority.Highest;
+            ChecksumsThread.Start();
+        }
+    }
+}
